42edcd6cc7cac1d3b87b54d118663e85a8f107f9
zodiac182 <zodiac182@outlook.com>
2019-05-06 16:39:38 +0800

    1st commit

---
+++ lianjia/items.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class LianjiaItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()    # 标题
+    # apartment = scrapy.Field()
+    # totalarea = scrapy.Field()
+    # availablearea = scrapy.Field()
+    floor = scrapy.Field()    # 楼层
+    # agelimit = scrapy.Field()
+    forsaledate = scrapy.Field()   # 挂牌时间
+    lastsaledate = scrapy.Field()  # 上次交易
+    community = scrapy.Field()   # 小区名称
+    area = scrapy.Field()  # 面积
+    areaName = scrapy.Field()  # 所在区域
+    areaName1 = scrapy.Field()  # 所在区域2
+    price = scrapy.Field()  # 总价
+    unitPrice = scrapy.Field()  # 单价
+    age = scrapy.Field()  # 房屋年限
+    url = scrapy.Field()  # url
---
+++ lianjia/spiders/lianjia.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from lianjia.items import LianjiaItem
+
+
+class LianjiaSpider(scrapy.Spider):
+    name = 'lianjia'
+    allowed_domains = ['lianjia.com']
+    start_urls = ['https://qd.lianjia.com/ershoufang/pg' + str(i) for i in range(1, 100)]
+
+    def parse(self, response):
+        for url in response.xpath('//div[@class="info clear"]//div[@class="title"]//a//@href'):
+            yield scrapy.Request(url=url.extract(), callback=self.parse_detail)
+
+    def parse_detail(self, response):
+        detail = LianjiaItem()
+        detail['title'] = ''.join(response.xpath('//div[@class="title"]//h1//@title').extract())
+        detail['community'] = ''.join(response.xpath('//div[@class="communityName"]//a[@class="info "]//text()').extract())
+        detail['areaName'] = response.xpath('//div[@class="areaName"]//span[@class="info"]//a//text()').extract()[0]
+        detail['areaName1'] = response.xpath('//div[@class="areaName"]//span[@class="info"]//a//text()').extract()[-1]
+        detail['price'] = ''.join(response.xpath('//div[@class="price "]//span[@class="total"]//text()').extract()) + u'万'
+        detail['unitPrice'] = ''.join(response.xpath('//div[@class="unitPrice"]//span[@class="unitPriceValue"]//text()').extract())
+        detail['area'] = ''.join(response.xpath('//div[@class="area"]//div[@class="mainInfo"]//text()').extract())
+        detail['forsaledate'] = ''.join(response.xpath(
+            '//div[@id="introduction"]//div//div[@class="introContent"]//div[@class="transaction"]//div[@class="content"]//ul//li[1]//span[2]//text()').extract())
+        detail['lastsaledate'] = ''.join(response.xpath(
+            '//div[@id="introduction"]//div//div[@class="introContent"]//div[@class="transaction"]//div[@class="content"]//ul//li[3]//span[2]//text()').extract())
+        detail['age'] = ''.join(response.xpath(
+            '//div[@id="introduction"]//div//div[@class="introContent"]//div[@class="transaction"]//div[@class="content"]//ul//li[5]//span[2]//text()').extract())
+        detail['floor'] = ''.join(response.xpath(
+            '//div[@id="introduction"]//div//div[@class="introContent"]//div[@class="base"]//div[@class="content"]//ul//li[2]//text()').extract())[4:]
+        detail['url'] = response.url
+        yield detail
